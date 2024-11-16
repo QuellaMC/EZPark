a090424addfb5b9270a024dbea7d3ea6080f309a
1jifang <55561815+1jifang@users.noreply.github.com>
2024-11-15 21:22:17 -0500

    Update config.py

--- Mac/src/utils/config.py
+++ Mac/src/utils/config.py
@@ -1,1 +1,9 @@
+import os
 
+class Config:
+    def __init__(self):
+        """
+        Initializes the configuration settings.
+        """
+        self.BASE_API_URL = os.getenv('EZPARK_API_URL', 'https://api.ezpark.com')
+        self.TIMEOUT = 10
