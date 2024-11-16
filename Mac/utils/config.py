ad251e9fad7b026b7888436b6f26cb70988acb11
1jifang <55561815+1jifang@users.noreply.github.com>
2024-11-15 21:22:50 -0500

    Delete Mac/utils directory

--- Mac/utils/config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-class Config:
-    def __init__(self):
-        """
-        Initializes the configuration settings.
-        """
-        self.BASE_API_URL = os.getenv('EZPARK_API_URL', 'https://api.ezpark.com')
-        self.TIMEOUT = 10
